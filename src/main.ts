740278589b083077c015c010ac2ef1bf51728554
Luan Honorio <110117312+luan-honorio@users.noreply.github.com>
2023-12-06 08:44:11 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const port = process.env.PORT ?? 3000
+  const port = process.env.PORT ?? 4000
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(port);
